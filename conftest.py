d6495b1f521253c8eac2e094c05d0256a430a106
Alexey Buchkin <a.buchkin@gmail.com>
2020-09-15 01:50:27 +0300

    add ftputil to project

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@
 import pytest
 import json
 import os.path
+import ftputil
 
 from fixture.application import Application
 from fixture.db import DbFixture
@@ -21,23 +22,25 @@ def load_config(file):
 
 
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
-    web_address = load_config(request.config.getoption("--target"))['web']
-    web_admin = load_config(request.config.getoption("--target"))['webadmin']
     browser = request.config.getoption("--browser")
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=web_address["baseUrl"])
-    fixture.session.login(username=web_admin["username"], password=web_admin["password"])
+        fixture = Application(browser=browser, base_url=config['web']["baseUrl"])
+    fixture.session.login(username=config['webadmin']["username"], password=config['webadmin']["password"])
     return fixture
 
 
 @pytest.fixture(scope="session")
-def db(request):
-    db_config = load_config(request.config.getoption("--target"))['db']
-    db = DbFixture(host=db_config['host'], name=db_config['name'],
-                          user=db_config['user'], password=db_config['password'])
+def db(request, config):
+    db = DbFixture(host=config['db']['host'], name=config['db']['name'],
+                          user=config['db']['user'], password=config['db']['password'])
     return db
+
+
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -48,6 +51,33 @@ def stop(request):
 
     request.addfinalizer(fin)
     return fixture
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_defaults_inc.bak"):
+            remote.remove("config_defaults_inc.bak")
+        if remote.path.isfile("config_defaults_inc.php"):
+            remote.rename("config_defaults_inc.php", "config_defaults_inc.bak")
+        remote.upload(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                   "resources/config_defaults_inc.php"), "config_defaults_inc.php")
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_defaults_inc.bak"):
+            if remote.path.isfile("config_defaults_inc.php"):
+                remote.remove("config_defaults_inc.php")
+            if remote.path.isfile("config_defaults_inc.php"):
+                remote.rename("config_defaults_inc.bak", "config_defaults_inc.php")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    def fin():
+        restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    request.addfinalizer(fin)
 
 
 def pytest_addoption(parser):
